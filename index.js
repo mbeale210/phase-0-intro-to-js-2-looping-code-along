0adfc993455b31cb02f3ee5bbe80f8aae517d0ad
Mark Beale <bealebeaverst@gmail.com>
2024-07-05 15:16:32 -0700

    Thank you cards

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function writeCards(names, gift) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${gift} gift!`;
+      thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+  }
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+   console.log(i);
+    }}
 // Code your solutions in this file
